12a590dd11a10b14dcdfafc81b5a696692c38787
KitE797 <kaeller@uvm.edu>
2026-03-19 14:14:52 -0400

    class 3/19/26

---
+++ r_scripts/python_intro.py
@@ -0,0 +1,188 @@
+# Intro to Python
+# More support and faster than R
+# Uses nesting instead of all of the brackets (aesthetics are important in the philosophy)
+# plotly package can make interactive graphs, also for graphs are matplotlib, seaborn
+# other packages include scipy, panda, numpy, etc.
+
+# Anatomy of a function
+# object = function(data = ds, method = "mean", num = 10) only difference from R is = instead of <-
+
+# Python objects have methods
+# object.python_method(method = "mean", num = 10)
+
+# Installing libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+# Importing package as abbreviated version - these are canonical abbreviations that you always use
+# Script language at heart so you generally write full script and just run it unless you're debugging but positron works like a debugger (line by line)
+
+# Have to explicitly state the package the function you're using is from so:
+# np.mean()
+
+###################################
+# Objects, Methods, and Functions
+###################################
+
+print("I love Python") # Always use print statement (R just uses it as a default but Python prefers explicit)
+
+greeting = "Hello"
+print(greeting)
+
+scalar = 6 # Integer value
+
+out = scalar * 3 # Doing math with an object
+print(out)
+
+myList = [34, 7, 98] # Creating a list
+
+myList.append(33) # Our first method, lists (object since not a vector language) have built in methods
+
+len(myList) # length function
+
+# data structures:
+#--------------------------------
+
+# make a list of colors
+a_list = ["blue", "green", "red"]
+print(a_list)
+
+# indexing into a list
+print(a_list[0]) # 0 is first element in list
+
+first_el = a_list[0]
+print(first_el)
+
+# looking at data types
+nums = [1, 2, 5, 8,]
+chars = ["a", "b", "c"]
+boolean = [True, True, False] # Not all caps or just T/F like R
+
+# mixed lists
+mixed = [1, 2, True, "blue", 5]
+
+# checking data types
+type(nums)
+type(nums[0])
+# type returns highest level object type so list over integer
+
+# negative indexing
+mixed[-1] # returns last element
+
+# ranged indexing
+mixed[1:4]
+mixed[:4] # Shows up to 4 places not to element 4 (3rd element)
+mixed[2:]
+
+# is an item in the list
+1 in mixed
+
+# changing elements
+mixed[4] = "green"
+
+# insert in a specific position without overwriting
+mixed.insert(0, "start")
+
+# other methods
+# extend, remove, pop (add to the end), clear (delete)
+
+
+
+print(mixed)
+
+[x for x in mixed]
+
+[x for x in mixed if isinstance(x,str)]
+
+
+####################
+# dictionaries
+
+
+
+
+
+
+
+
+dataTypes = {
+    "string":"thing",
+    "integer":3,
+    "float":3.1432,
+    "list":[1,2,3,"a"],
+    "boolean":False
+}
+
+# calling values by key name in brackets
+dataTypes["string"]
+
+dataTypes.get("boolean")
+
+dataTypes.keys()
+dataTypes.values()
+dataTypes.items()
+
+dataTypes["age"] = 35
+
+
+
+
+arr1 = np.array([0,1,2,3,4,5,6,7,8,9])
+
+arr1[3]
+arr1[-1]
+arr1[:3]
+
+arr1[1:8:4] # last place in indexing encodes every nth place in array
+
+# 2d array
+arr2 = np.array([[1,2,3], [4,5,6], [7,8,9]])
+
+# 3d array
+arr3 = np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
+
+arr3[1,0,1]
+
+arr1.ndim()
+arr2.ndim()
+arr3.ndim()
+
+
+arr1.shape()
+arr2.shape()
+arr3.shape()
+
+arr1.type()
+arr2.type()
+arr3.type()
+
+
+first = np.array([1,2,3])
+second = np.array([4,5,6])
+
+long_array = np.concatenate((first, second))
+
+np.concatenate((arr2, arr2), axis = 1)
+
+np.array_split(arr1, 2)
+
+
+
+
+
+### random numbers
+ from numpy import random
+
+random.seed(seed = 100)
+
+randomInt = random.randint(50) # gives 1 int from 0 to 50
+
+random.rand(50) # gives 50 values 0 to 1
+
+random.rand(50, 5, 10)
+
+random.choice(arr1) # random number from arr1
+
+random.choice(arr1, size = (3,3))
+
+random.choice([0,1], p = [0.01, 0.99], size = 1000)
